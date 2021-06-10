33657e994caff11875354dfd50838e11b18f0005
Rodrigo <rodrigouribe.gori@gmail.com>
2021-06-09 20:29:09 -0600

    Dataset

--- SistemaPlanilla/Logica/Properties/Settings.Designer.cs
+++ SistemaPlanilla/Logica/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Logica.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,16 @@ namespace Logica.Properties {
         public string BD_PLANILLAConnectionString {
             get {
                 return ((string)(this["BD_PLANILLAConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQL2019;Initial Catalog=BD_PLANILLA;Integrated Security=True")]
+        public string BD_PLANILLA2 {
+            get {
+                return ((string)(this["BD_PLANILLA2"]));
             }
         }
     }
